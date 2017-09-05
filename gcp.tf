e93a0de2b29aa05cfd04a191b0d9bdd9109b5d8c
Rob Horner <robert@horners.org>
2017-09-05 14:19:35 -0700

    make GCP-AWS multi-tunnel

--- gcp.tf
+++ gcp.tf
@@ -32,28 +32,57 @@ resource "google_compute_router" "cloud-router" {
 
 }
 
-resource "google_compute_router_interface" "interface-1" {
-  name = "interface-1"
+# Create Cloud Router interface and BGP peer for first AWS VPN link
+resource "google_compute_router_interface" "interface-aws-1" {
+  name = "interface-aws-1"
   router     = "${google_compute_router.cloud-router.name}"
-//  ip_range   = "169.254.1.1/30"
   ip_range   = "${aws_vpn_connection.gcp-vpn-connection.tunnel1_cgw_inside_address}/30"
-  vpn_tunnel = "${google_compute_vpn_tunnel.tunnel-to-aws.name}"
+  vpn_tunnel = "${google_compute_vpn_tunnel.tunnel1-to-aws.name}"
 }
 
-resource "google_compute_router_peer" "aws_vpn_peer" {
-  name                      = "aws-vpn-peer"
+resource "google_compute_router_peer" "aws_vpn_peer-1" {
+  name                      = "aws-vpn-peer-1"
   router                    = "${google_compute_router.cloud-router.name}"
   peer_ip_address           = "${aws_vpn_connection.gcp-vpn-connection.tunnel1_vgw_inside_address}"
   peer_asn                  = 7224
   advertised_route_priority = 100
-  interface                 = "${google_compute_router_interface.interface-1.id}"
+  interface                 = "${google_compute_router_interface.interface-aws-1.id}"
 }
 
-# Define the VPN tunnel to AWS. Include the local address range that should be sent toward the tunnel.
-resource "google_compute_vpn_tunnel" "tunnel-to-aws" {
-  name               = "tunnel-to-aws"
+# Create Cloud Router interface and BGP peer for second AWS VPN link
+
+resource "google_compute_router_interface" "interface-aws-2" {
+  name = "interface-aws-2"
+  router     = "${google_compute_router.cloud-router.name}"
+  ip_range   = "${aws_vpn_connection.gcp-vpn-connection.tunnel2_cgw_inside_address}/30"
+  vpn_tunnel = "${google_compute_vpn_tunnel.tunnel2-to-aws.name}"
+}
+
+resource "google_compute_router_peer" "aws_vpn_peer-2" {
+  name                      = "aws-vpn-peer-2"
+  router                    = "${google_compute_router.cloud-router.name}"
+  peer_ip_address           = "${aws_vpn_connection.gcp-vpn-connection.tunnel2_vgw_inside_address}"
+  peer_asn                  = 7224
+  advertised_route_priority = 100
+  interface                 = "${google_compute_router_interface.interface-aws-2.id}"
+}
+
+# Define the VPN tunnels to AWS. Include the local address range that should be sent toward the tunnel.
+resource "google_compute_vpn_tunnel" "tunnel1-to-aws" {
+  name               = "tunnel1-to-aws"
   peer_ip            = "${aws_vpn_connection.gcp-vpn-connection.tunnel1_address}"
   shared_secret      = "${aws_vpn_connection.gcp-vpn-connection.tunnel1_preshared_key}"
+  target_vpn_gateway = "${google_compute_vpn_gateway.gcp-vpn-gateway.self_link}"
+
+  ike_version = 1
+
+  router             = "${google_compute_router.cloud-router.name}"
+}
+
+resource "google_compute_vpn_tunnel" "tunnel2-to-aws" {
+  name               = "tunnel2-to-aws"
+  peer_ip            = "${aws_vpn_connection.gcp-vpn-connection.tunnel2_address}"
+  shared_secret      = "${aws_vpn_connection.gcp-vpn-connection.tunnel2_preshared_key}"
   target_vpn_gateway = "${google_compute_vpn_gateway.gcp-vpn-gateway.self_link}"
 
   ike_version = 1
